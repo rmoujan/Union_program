eb26a513abd5239b870ddd99631d740f59d671d7
Rachida Moujan <rmoujan@e1r11p6.1337.ma>
2022-01-07 18:13:58 +0100

    version 3 is working but not complet

--- union.c
+++ union.c
@@ -6,28 +6,31 @@
 /*   By: rmoujan <rmoujan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 07:47:45 by rmoujan           #+#    #+#             */
-/*   Updated: 2022/01/07 18:03:34 by rmoujan          ###   ########.fr       */
+/*   Updated: 2022/01/07 18:12:15 by rmoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 //#include <stdio.h>
 
-int ft_check(int i, char *ptr, int k)
+int ft_check(int i, int j, char *ptr)
 {
-    int j;
+    int k;
     
-    j = 0;
-    if (i == k)
-        return (1);
-    while (ptr[j] != '\0' && j <= i)
-   {
-       if (ptr[j] == ptr[i])
-       {
-           return (0);
-       }
-    j++;
-   }
+    k = 0;
+    if (i == j && ptr[i] == ptr[j])
+    {
+		//check is the char already exit or not
+        while (ptr[k] != '\0' && k < i)
+        {
+            if (ptr[k] == ptr[i])
+            {
+                return (0);
+            }
+            k++;
+        }
+    }
+
    return (1);
 }
 
@@ -45,12 +48,12 @@ int main(int argc, char *argv[])
 	   	while (argv[1][j] != '\0')
 	   	{
 
-	   	    if (argv[1][i] == argv[1][j] && i >= j && ft_check(i, argv[1], j) == 1)
+	   	    if (argv[1][i] == argv[1][j] && i >= j)
 	   	    {
-	   	       //printf(" ft_check == %d \n",ft_check(i, argv[1]));
-
-	   	        //printf(" i and j equal zero i == %d and j == %d \n",i,j);
-	   	        write(1, &argv[1][i], 1);
+	   	        if (ft_check(i, j, argv[1]) == 1)
+	   	        {
+	   	            write(1, &argv[1][i], 1);
+	   	        }
 	   	    }
 	   	   j++;
 	   	} 
