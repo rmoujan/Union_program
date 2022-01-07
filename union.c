eac08880540e2ca1a7a86d35f1e600757c7b755a
Rachida Moujan <rmoujan@e1r11p6.1337.ma>
2022-01-07 18:05:11 +0100

    version 2 of union not working

--- union.c
+++ union.c
@@ -6,47 +6,56 @@
 /*   By: rmoujan <rmoujan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 07:47:45 by rmoujan           #+#    #+#             */
-/*   Updated: 2022/01/05 13:07:15 by rmoujan          ###   ########.fr       */
+/*   Updated: 2022/01/07 18:03:34 by rmoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
+//#include <stdio.h>
+
+int ft_check(int i, char *ptr, int k)
+{
+    int j;
+    
+    j = 0;
+    if (i == k)
+        return (1);
+    while (ptr[j] != '\0' && j <= i)
+   {
+       if (ptr[j] == ptr[i])
+       {
+           return (0);
+       }
+    j++;
+   }
+   return (1);
+}
 
 int main(int argc, char *argv[])
 {
 	int i;
 	int j;
-	int flag;
-	printf("%d\n",argc);
-    if (argc <= 2)
-	{
-		write(1, "r\n", 2);
-		return (0);
-	}
+
 	i = 0;
 	j = 0;
-	flag = 1;
-	//halhhloh -- >halo
+
 	while (argv[1][i] != '\0')
 	{
-		while (argv[1][j] != '\0')
-		{
-			if (argv[1][j] == argv[1][i])
-			{
-				if (flag == 1)
-				{ 
-					//printf("yes \n");
-					write (1, &argv[1][i], 1);
-					flag++;
-				}
-			}
-			
-		j++;
-		}
-	//flag = 1;
-	j = 0;
-	i++;
+	    j = i;
+	   	while (argv[1][j] != '\0')
+	   	{
+
+	   	    if (argv[1][i] == argv[1][j] && i >= j && ft_check(i, argv[1], j) == 1)
+	   	    {
+	   	       //printf(" ft_check == %d \n",ft_check(i, argv[1]));
+
+	   	        //printf(" i and j equal zero i == %d and j == %d \n",i,j);
+	   	        write(1, &argv[1][i], 1);
+	   	    }
+	   	   j++;
+	   	} 
+	   	i++;
 	}
+
 	return (0);
 }
