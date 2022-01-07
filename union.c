c0ecd073f85b731ed7f32bc9b574b4f150fbad69
Rachida Moujan <rmoujan@e1r11p6.1337.ma>
2022-01-07 18:26:44 +0100

    version 3 working but not complet

--- union.c
+++ union.c
@@ -6,31 +6,34 @@
 /*   By: rmoujan <rmoujan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 07:47:45 by rmoujan           #+#    #+#             */
-/*   Updated: 2022/01/07 18:15:46 by rmoujan          ###   ########.fr       */
+/*   Updated: 2022/01/07 18:20:44 by rmoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 //#include <stdio.h>
 
-int ft_check(int i, char *ptr, int j)
+#include <unistd.h>
+#include <stdio.h>
+
+int ft_check(int i, int j, char *ptr)
 {
     int k;
     
     k = 0;
     if (i == j && ptr[i] == ptr[j])
     {
-        // check is the char already exit or not
-        return (1);
+		//check is the char already exits or not
+        while (ptr[k] != '\0' && k < i)
+        {
+            if (ptr[k] == ptr[i])
+            {
+                return (0);
+            }
+            k++;
+        }
     }
-    while (ptr[k] != '\0' && k <= i)
-   {
-       if (ptr[k] == ptr[i])
-       {
-           return (0);
-       }
-    k++;
-   }
+
    return (1);
 }
 
@@ -44,20 +47,16 @@ int main(int argc, char *argv[])
 
 	while (argv[1][i] != '\0')
 	{
-	    j = 0;
+	    j = i;
 	   	while (argv[1][j] != '\0')
 	   	{
 
 	   	    if (argv[1][i] == argv[1][j] && i >= j)
 	   	    {
-	   	        if (ft_check(i, argv[1], j) == 1)
+	   	        if (ft_check(i, j, argv[1]) == 1)
 	   	        {
 	   	            write(1, &argv[1][i], 1);
 	   	        }
-	   	       //printf(" ft_check == %d \n",ft_check(i, argv[1]));
-
-	   	        //printf(" i and j equal zero i == %d and j == %d \n",i,j);
-	   	        
 	   	    }
 	   	   j++;
 	   	} 
